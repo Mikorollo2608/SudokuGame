4183b8b440d47fef4615c00f1c3a5efba08a13f7
Jakub Kozielski <242438@edu.p.lodz.pl>
2022-10-21 18:10:09 +0200

    Changing implementation of fillBoard in SudokuBoard to account for partially filled boards, adding new tests

--- src/main/java/org/example/SudokuBoard.java
+++ src/main/java/org/example/SudokuBoard.java
@@ -1,15 +1,26 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Stack;
 
 public class SudokuBoard {
     private int[][] board = new int[9][9];
 
-    /** Getter to a single cell on the board.
+    {
+        //initialize the board with 0
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    /**
+     * Getter to a single cell on the board.
      *
-     * @param row   Row of the board.
-     * @param col   Column of the board.
-     * @return      Asked for cell
+     * @param row Row of the board.
+     * @param col Column of the board.
+     * @return Asked for cell
      */
     public int get(int row, int col) {
         return board[row][col];
@@ -21,29 +32,6 @@ public class SudokuBoard {
 
     public void fillBoard() {
         Random random = new Random();
-
-        //fill the board with 0
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                board[i][j] = 0;
-            }
-        }
-
-        //generate first row
-        for (int i = 0; i < 9; i++) {
-            while (board[0][i] == 0) {
-                int temp = random.nextInt(9) + 1;
-                int j = 0;
-                for (; j < i; j++) {
-                    if (temp == board[0][j]) {
-                        break;
-                    }
-                }
-                if (j == i) {
-                    board[0][i] = temp;
-                }
-            }
-        }
 
         //shuffle the priorities of numbers
         int[] numbers = new int[9];
@@ -63,22 +51,35 @@ public class SudokuBoard {
         }
 
         //starting from 2nd row
-        int row = 1;
+        int row = 0;
         int col = 0;
+
+        //holds row,column and index of previously tested cell
+        //idex being index of tested number from an array numbers
+        Stack<Integer> changesStack = new Stack<>();
+        //flag indicating that the solver is takng a step back
+        boolean back = false;
+        //index from which start trying numbers
+        int startingIndex = 0;
+
 
         //try to fit numbers until you reach out of the board
         while (row != 9) {
 
-            //find first untried number in array of numbers
-            int startingIndex = 0;
-            for (int i = 0; i < 9; i++) {
-                if (board[row][col] == numbers[i]) {
-                    startingIndex = i + 1;
-                    break;
+            //if the cell is not empty and you're not taking a step back go to the next cell
+            if (board[row][col] != 0 && !back) {
+                col++;
+                if (col == 9) {
+                    row++;
+                    col = 0;
                 }
+                continue;
             }
 
-            //try fitting numbers
+            //reset back flag
+            back = false;
+
+            //try fitting numbers one by one from numbers array
             int i = startingIndex;
             for (; i < 9; i++) {
 
@@ -112,24 +113,32 @@ public class SudokuBoard {
 
                 //if the numbers fits set it on the board and move to the next square
                 if (isValid) {
+
+                    //add current cell to the changes stack
+                    changesStack.push(row);
+                    changesStack.push(col);
+                    changesStack.push(i);
+                    startingIndex = 0;
                     board[row][col] = numbers[i];
                     col++;
                     if (col == 9) {
                         row++;
                         col = 0;
                     }
+
                     break;
                 }
             }
             //if you reached the end of the loop, then none of the tried numbers fit
-            //move one square back
+            //move to the previously changed cell
             if (i == 9) {
+                back = true;
                 board[row][col] = 0;
-                col--;
-                if (col == -1) {
-                    row--;
-                    col = 8;
-                }
+                //take previous cell information from changesStack
+                startingIndex = changesStack.pop();
+                col = changesStack.pop();
+                row = changesStack.pop();
+
             }
         }
     }
--- src/test/java/org/example/SudokuBoardTest.java
+++ src/test/java/org/example/SudokuBoardTest.java
@@ -14,15 +14,15 @@ class SudokuBoardTest {
         SudokuBoard sudokuBoard = new SudokuBoard();
         Random random = new Random();
         int testValue = random.nextInt(9)+1;
-        int row = random.nextInt(9)+1;
-        int column = random.nextInt(9)+1;
+        int row = random.nextInt(9);
+        int column = random.nextInt(9);
 
         sudokuBoard.set(row,column,testValue);
         assertEquals(sudokuBoard.get(row,column),testValue);
     }
 
     @Test
-    void fillBoard() {
+    void fillBoardEmptyTest() {
         SudokuBoard sudokuBoard = new SudokuBoard();
         sudokuBoard.fillBoard();
 
@@ -80,13 +80,14 @@ class SudokuBoardTest {
             }
         }
 
-        //testing 2 different layouts for 2 consecutive calls fillBoard
+        //testing 2 different layouts for 2 consecutive calls of fillBoard on different objects
+        SudokuBoard sudokuBoard1 = new SudokuBoard();
         sudokuBoard.fillBoard();
         int[][] testArray2 = new int[9][9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                testArray2[i][j] = sudokuBoard.get(i, j);
+                testArray2[i][j] = sudokuBoard1.get(i, j);
             }
         }
 
@@ -115,6 +116,76 @@ class SudokuBoardTest {
 
         assertFalse(Arrays.deepEquals(testArray2, testArray3));
 
+    }
+
+    @Test
+    void fillBoardPartiallyFilledTest(){
+        SudokuBoard sudokuBoard = new SudokuBoard();
+        Random random = new Random();
+        int testRow = random.nextInt(9);
+        int testColumn = random.nextInt(9);
+        int testValue = random.nextInt(9)+1;
+
+        sudokuBoard.set(testRow,testColumn,testValue);
+
+        sudokuBoard.fillBoard();
+
+        int[][] testArray = new int[9][9];
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                testArray[i][j] = sudokuBoard.get(i, j);
+            }
+        }
+
+        //testing rows
+        while (row != 9) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = i + 1; j < 9; j++) {
+                    assertNotEquals(testArray[row][j], testArray[row][i]);
+                }
+            }
+            row++;
+        }
+
+        //testing columns
+        while (col != 9) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = i + 1; j < 9; j++) {
+                    assertNotEquals(testArray[j][col], testArray[i][col]);
+                }
+            }
+            col++;
+        }
+
+        //testing boxes
+        row = col = 0;
+        int tmp;
+
+        while (row < 9) {
+            for (int i = row; i < row + 3; i++) {
+                for (int j = col; j < col + 3; j++) {
+                    tmp = testArray[i][j];
+                    testArray[i][j] = 0;
+                    for (int a = i; a < row + 3; a++) {
+                        for (int b = j; b < col + 3; b++) {
+                            assertNotEquals(tmp, testArray[a][b]);
+                        }
+                    }
+                    testArray[i][j] = tmp;
+                }
+            }
+            col += 3;
+            if (col == 9) {
+                col = 0;
+                row += 3;
+            }
+        }
+
+        //check if the testValue set before is still the same
+        assertEquals(sudokuBoard.get(testRow,testColumn),testValue);
     }
 
 }
