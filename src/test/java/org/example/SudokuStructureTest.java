36f0bf1de8b0d523a288c4f9ae9f48d5b9ef3b39
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2022-11-13 15:57:52 +0100

    Changed arrays in SudokuStructureTest to collections and used shuffle method instead of our own randomize algorithm

--- src/test/java/org/example/SudokuStructureTest.java
+++ src/test/java/org/example/SudokuStructureTest.java
@@ -4,10 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-
-
 
 class SudokuStructureTest {
 
@@ -17,34 +18,19 @@ class SudokuStructureTest {
 
         SudokuField[] board = new SudokuField[9];
 
-        //Create table with shuffled numbers from range (0;9>
-        Random random = new Random();
-
-        int[] tab = new int[9];
-        for (int i = 0; i < 9; i++) {
-            while (tab[i] == 0) {
-                int temp = random.nextInt(9) + 1;
-                int j = 0;
-                for (; j < i; j++) {
-                    if (temp == tab[j]) {
-                        break;
-                    }
-                }
-                if (j == i) {
-                    tab[i] = temp;
-                }
-            }
-        }
+        //Create list with shuffled numbers from range (0;9>
+        List<Integer> tab = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        Collections.shuffle(tab);
 
         for (int i = 0; i < 9; i++) {
             board[i] = new SudokuField();
-            board[i].setFieldValue(tab[i]);
+            board[i].setFieldValue(tab.get(i));
             sudokuStructure.setContents(board[i], i);
         }
 
 
         for (int i = 0; i < 9; i++) {
-            assertEquals(sudokuStructure.get(i).getFieldValue(), tab[i]);
+            assertEquals(sudokuStructure.get(i).getFieldValue(), tab.get(i));
         }
 
     }
@@ -56,53 +42,25 @@ class SudokuStructureTest {
 
         SudokuField[] board = new SudokuField[9];
 
-        //Create table with shuffled numbers from range (0;9>, without repetitions
-        Random random = new Random();
+        //Create list with shuffled numbers from range (0;9>, without repetitions
+        List<Integer> tabTrue = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        Collections.shuffle(tabTrue);
 
-        int[] tabTrue = new int[9];
-        for (int i = 0; i < 9; i++) {
-            while (tabTrue[i] == 0) {
-                int temp = random.nextInt(9) + 1;
-                int j = 0;
-                for (; j < i; j++) {
-                    if (temp == tabTrue[j]) {
-                        break;
-                    }
-                }
-                if (j == i) {
-                    tabTrue[i] = temp;
-                }
-            }
-        }
-
-        //Create table with shuffled numbers from range (0;9>, with one repetition
-        int[] tabFalse = new int[9];
-        for (int i = 0; i < 9; i++) {
-            while (tabFalse[i] == 0) {
-                int temp = random.nextInt(9) + 1;
-                int j = 0;
-                for (; j < i; j++) {
-                    if (temp == tabFalse[j]) {
-                        break;
-                    }
-                }
-                if (j == i) {
-                    tabFalse[i] = temp;
-                }
-            }
-        }
-        tabFalse[3] = tabFalse[4];
+        //Create list with shuffled numbers from range (0;9>, with one repetition
+        List<Integer> tabFalse = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        Collections.shuffle(tabFalse);
+        tabFalse.set(3,tabFalse.get(4));
 
         for (int i = 0; i < 9; i++) {
             board[i] = new SudokuField();
-            board[i].setFieldValue(tabTrue[i]);
+            board[i].setFieldValue(tabTrue.get(i));
             sudokuStructure.setContents(board[i], i);
         }
 
         assertTrue(sudokuStructure.verify());
 
         for (int i = 0; i < 9; i++) {
-            board[i].setFieldValue(tabFalse[i]);
+            board[i].setFieldValue(tabFalse.get(i));
             sudokuStructure.setContents(board[i], i);
         }
 
