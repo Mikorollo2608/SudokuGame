9481a1a7f5c0a17a4364575b1b9434b3bb566345
Jakub Kozielski <242438@edu.p.lodz.pl>
2022-11-28 21:06:22 +0100

    reseting View

--- View/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.view {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens org.example.view to javafx.fxml;
-    exports org.example.view;
-}
