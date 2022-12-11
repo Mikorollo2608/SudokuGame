cc8bad266a0463763357e44c6580ee016174fa77
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2022-12-11 16:42:45 +0100

    Connected displaying sudoku board with choosing difficulty

--- View/src/main/java/com/example/view/DisplaySudokuController.java
+++ View/src/main/java/com/example/view/DisplaySudokuController.java
@@ -7,31 +7,39 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import org.example.BacktrackingSudokuSolver;
+import org.example.Difficulty;
 import org.example.SudokuBoard;
 
-public class PrintSudokuController {
+public class DisplaySudokuController {
 
     private SudokuBoard sudokuBoard;
 
     private TextField[] textFields;
 
+    private Difficulty chosenLevel;
     @FXML
     private GridPane grid;
 
-    public PrintSudokuController() {
+    public DisplaySudokuController() {
         sudokuBoard = new SudokuBoard(new BacktrackingSudokuSolver());
-        sudokuBoard.solveGame();
         textFields = new TextField[81];
     }
 
-    @FXML
-    private void initialize() {
+    public void display(Difficulty lvl) {
+        sudokuBoard.solveGame();
+        chosenLevel = lvl;
+        chosenLevel.removeFields(sudokuBoard);
         int i = 0;
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
+                if (sudokuBoard.get(row, col) == 0) {
+                    i++;
+                    continue;
+                }
                 textFields[i] = new TextField();
                 textFields[i].setAlignment(Pos.CENTER);
                 textFields[i].setFont(Font.font("Verdana", FontWeight.BOLD, 36));
+                textFields[i].setEditable(false);
                 textFields[i].setText(Integer.toString(sudokuBoard.get(row, col)));
                 grid.add(textFields[i], col, row, 1, 1);
                 i++;
--- View/src/main/java/com/example/view/SceneController.java
+++ View/src/main/java/com/example/view/SceneController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.Difficulty;
 
 
 public class SceneController {
@@ -15,12 +16,34 @@ public class SceneController {
     private Scene scene;
     private Parent root;
 
+    private Difficulty chosenLevel;
+
     public void switchToSudokuBoardScene(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("sudokuBoardScene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sudokuBoardScene.fxml"));
+        root = loader.load();
+
+        DisplaySudokuController displaySudokuController = loader.getController();
+        displaySudokuController.display(chosenLevel);
+
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void pressedEasyButton(ActionEvent event) throws IOException {
+        chosenLevel = Difficulty.EASY;
+        switchToSudokuBoardScene(event);
+    }
+
+    public void pressedMediumButton(ActionEvent event) throws IOException {
+        chosenLevel = Difficulty.MEDIUM;
+        switchToSudokuBoardScene(event);
+    }
+
+    public void pressedHardButton(ActionEvent event) throws IOException {
+        chosenLevel = Difficulty.HARD;
+        switchToSudokuBoardScene(event);
     }
 
 }
