55ef1b61e9b1318e5bd260db2ce545ba5c355d7b
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2022-12-16 00:42:58 +0100

    Used TextFormatter instead of SudokuTextField with overridden method

--- View/src/main/java/com/example/view/DisplaySudokuController.java
+++ View/src/main/java/com/example/view/DisplaySudokuController.java
@@ -1,18 +1,22 @@
 package com.example.view;
 
+import java.util.function.UnaryOperator;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import org.example.BacktrackingSudokuSolver;
 import org.example.SudokuBoard;
 
+
 public class DisplaySudokuController {
 
     private SudokuBoard sudokuBoard;
 
-    private SudokuTextField[] textFields;
+    private TextField[] textFields;
 
     private Difficulty chosenLevel;
     @FXML
@@ -20,7 +24,7 @@ public class DisplaySudokuController {
 
     public DisplaySudokuController() {
         sudokuBoard = new SudokuBoard(new BacktrackingSudokuSolver());
-        textFields = new SudokuTextField[81];
+        textFields = new TextField[81];
     }
 
     public void display(Difficulty lvl) {
@@ -30,14 +34,24 @@ public class DisplaySudokuController {
         int i = 0;
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                textFields[i] = new SudokuTextField();
+                textFields[i] = new TextField();
                 textFields[i].setAlignment(Pos.CENTER);
                 textFields[i].setFont(Font.font("Verdana", FontWeight.BOLD, 36));
                 if (sudokuBoard.get(row, col) == 0) {
                     textFields[i].setText("");
 
+                    UnaryOperator<TextFormatter.Change> sudokuFieldFormat = change -> {
+                        String newText = change.getControlNewText();
+                        if (newText.matches("[1-9]?")) {
+                            return change;
+                        }
+                        return null;
+                    };
+
+                    textFields[i].setTextFormatter(new TextFormatter<String>(sudokuFieldFormat));
+
                     textFields[i].textProperty().addListener((observable, oldValue, newValue) -> {
-                        System.out.println("textfield changed from " + oldValue + " to " + newValue);
+                        System.out.println("Change from " + oldValue + " to " + newValue);
                     });
 
                 } else {
