d6f96e576fa6adbf1a197f7dbcf2138469463fc8
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2022-12-19 14:10:40 +0100

    One additional file to last commit

--- View/src/main/java/com/example/view/Application.java
+++ View/src/main/java/com/example/view/Application.java
@@ -1,6 +1,7 @@
 package com.example.view;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,8 +10,12 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("levelScene.fxml"));
+
+        ResourceBundle bundle = ResourceBundle.getBundle("com.example.view.MyBundle");
+        fxmlLoader.setResources(bundle);
+
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("SudokuGame");
+        stage.setTitle(bundle.getString("title"));
         stage.setScene(scene);
         stage.show();
     }
